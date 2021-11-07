684f27da19e7e261868edc961e72c9ecc1a338f5
Devon Morris <devonmorris1992@gmail.com>
2021-11-07 12:59:33 -0500

    updated keymap for iris to match more closely to corne (and for gaming)

--- keymap.c
+++ keymap.c
@@ -32,13 +32,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              RGB_TOG,  RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_EQL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL,
+      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5 ,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11,\
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS, LALT(KC_1),LALT(KC_2),LALT(KC_3),LALT(KC_4),LALT(KC_5),                 KC_LEFT, KC_DOWN,  KC_UP,   KC_RIGHT,  LALT(KC_0), KC_TRNS,
+      KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, KC_MPLY, KC_TRNS,\
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT,LALT(KC_6),LALT(KC_7),LALT(KC_8),LALT(KC_9),LALT(KC_0),KC_SPC,KC_ENT,    KC_MUTE,KC_VOLD, KC_VOLU,  KC_TRNS, KC_TRNS, KC_RSFT,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_SPC,            KC_HOME, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_RSFT,\
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, KC_TRNS, KC_SPC,                    KC_ENT,  KC_TRNS, KC_RALT
+                                    KC_TRNS, KC_TRNS, KC_TRNS,                    KC_HOME, KC_END, KC_LGUI \
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -46,13 +46,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+      KC_GRV,   KC_1,    KC_2,    KC_3,   KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,\
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_LBRC,  KC_LCBR, KC_LPRN, KC_PPLS, KC_HOME, KC_PLUS,
+      KC_GESC, KC_EXLM,  KC_AT,  KC_HASH, KC_DLR, KC_PERC,                             KC_LBRC, KC_LCBR, KC_LPRN, KC_PPLS, KC_MINS, KC_PLUS,\
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_CIRC, KC_AMPR, KC_ASTR, KC_TRNS, KC_TRNS, KC_TRNS,          _______, KC_RBRC,  KC_RCBR, KC_RPRN, KC_EQL,  KC_END, _______,
+     KC_LSFT, KC_CIRC, KC_AMPR, KC_ASTR, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_RBRC,  KC_RCBR, KC_RPRN, KC_EQL,  KC_END, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
+                                    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_RALT, KC_DEL
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -109,21 +109,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
   return true;
-}
-
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    }
 }
